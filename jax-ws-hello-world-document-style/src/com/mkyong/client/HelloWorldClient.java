9e41ae0a328c29dac01389c18415c1b63906bf36
acuevas <acuevas@localhost>
2017-05-09 13:56:00 -0500

    JAX-WS HELLO WORLD DOCUMENT STYLE

--- jax-ws-hello-world-document-style/src/com/mkyong/client/HelloWorldClient.java
+++ jax-ws-hello-world-document-style/src/com/mkyong/client/HelloWorldClient.java
@@ -10,7 +10,8 @@ import com.mkyong.ws.HelloWorld;
 public class HelloWorldClient {
 
 	public static void main(String[] args) throws Exception {
-
+//      SI QUIERES UTILIZAR EL TCP/IP MONITOR CAMBIAS EL PUERTO POR EL CONFIGURADO EN EL TCP/IP MONITOR
+//		URL url = new URL("http://localhost:8888/ws/hello?wsdl");
 		URL url = new URL("http://localhost:9999/ws/hello?wsdl");
 		QName qname = new QName("http://ws.mkyong.com/", "HelloWorldImplService");
 		Service service = Service.create(url, qname);
