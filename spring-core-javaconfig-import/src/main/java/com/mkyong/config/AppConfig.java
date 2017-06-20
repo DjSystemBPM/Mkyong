e8a5db08707f6c358ba90ca149898b0960b632c8
acuevas <acuevas@localhost>
2017-06-20 16:13:50 -0500

    SPRING CORE JAVA CONFIG IMPORT

---
+++ spring-core-javaconfig-import/src/main/java/com/mkyong/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.mkyong.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({ CustomerConfig.class, SchedulerConfig.class })
+public class AppConfig {
+
+	
+}
